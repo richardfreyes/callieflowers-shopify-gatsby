d3419f63189a4961cf115f1aac9d67478c09b923
Richard Reyes <richard.frey@gus.global>
2021-12-11 20:31:25 +0800

    api testing 3

--- src/shared/context/store-context.js
+++ src/shared/context/store-context.js
@@ -3,7 +3,7 @@ import fetch from "isomorphic-fetch"
 import Client from "shopify-buy"
 
 console.log('process.env', process.env)
-console.log('process.env.SHOPIFY_API_KEY', process.env.SHOPIFY_API_KEY)
+console.log('process.env.GATSBY_SHOPIFY_STORE_URL', process.env.GATSBY_SHOPIFY_STORE_URL)
 
 
 
